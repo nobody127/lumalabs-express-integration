b3070c0abf69d7c06299b221c419b76b88fe59a2
uditrajmr3 <uditrajmr3@gmail.com>
2025-02-21 06:06:31 +0000

    --updated: tests - server on caught error handlers and pretty print error handler

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//   testEnvironment: 'node',
+//   moduleDirectories: ['node_modules', 'src'],
+// };
+module.exports = {
+  testEnvironment: 'node',
+  setupFiles: ['<rootDir>/jest.setup.js'], // Point to the Jest setup file
+};
--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+// external or 3rd party imports here
 const express = require('express');
+// internal imports here
 
+// constants and variables definitions here
 const app = express();
 
 module.exports = app;
